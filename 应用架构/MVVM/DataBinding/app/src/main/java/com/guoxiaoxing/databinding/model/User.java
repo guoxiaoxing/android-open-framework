31549b3f8071bee53605c16c5f0fbca061dcc5fc
guoxiaoxing <guoxiaoxingv@gmail.com>
2016-02-01 21:20:42 +0800

    add databing project

---
+++ 应用架构/MVVM/DataBinding/app/src/main/java/com/guoxiaoxing/databinding/model/User.java
@@ -0,0 +1,22 @@
+package com.guoxiaoxing.databinding.model;
+
+/**
+ * Created by guoxiaoxing on 16-2-1.
+ */
+public class User {
+    private final String firstName;
+    private final String lastName;
+
+    public User(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+}
